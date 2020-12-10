090d3ba3a25b049c67dbd2053749ea5c65cb352c
Yun Yeo <sw.yunsuk@gmail.com>
2020-12-10 21:49:30 +0900

    ignore invalid project id error

--- eth/src/Shuttle.ts
+++ eth/src/Shuttle.ts
@@ -54,9 +54,6 @@ class Shuttle {
 
     while (!shutdown) {
       await this.process().catch(async (err) => {
-        // ignore invalid project id
-        if (err.message === 'invalid project id\n') return;
-
         const errorMsg =
           err instanceof Error ? err.toString() : JSON.stringify(err);
         console.error(`Process failed: ${errorMsg}`);
