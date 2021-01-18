8ed0fdf559e082ee0cbab190e14c39c97fb4893d
Yun Yeo <sw.yunsuk@gmail.com>
2021-01-18 18:07:07 +0900

    clear logging

--- terra/src/index.ts
+++ terra/src/index.ts
@@ -6,17 +6,16 @@ const shuttle = new Shuttle();
 
 console.info(`
 Start ShuttleTerra
-\n
+
 TERRA_CHAIN_ID: ${process.env.TERRA_CHAIN_ID}
 ETH_CHAIN_ID: ${process.env.ETH_CHAIN_ID}
-\n
+
 TERRA_TRACKING_ADDR: ${process.env.TERRA_TRACKING_ADDR}
-\n
+
 FEE_RATE: ${process.env.FEE_RATE}
 FEE_MIN_AMOUNT: ${process.env.FEE_MIN_AMOUNT}
 FEE_QUOTE_TICKER: ${process.env.FEE_QUOTE_TICKER}
 -----------------------------------------------------------
-\n\n
 `);
 
 shuttle.startMonitoring().catch((err) => {
