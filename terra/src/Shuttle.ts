1447f16b7e06488ba0598e4542989ff839ef6da9
Yun Yeo <sw.yunsuk@gmail.com>
2021-04-14 23:14:22 +0900

    add error handling

--- terra/src/Shuttle.ts
+++ terra/src/Shuttle.ts
@@ -181,7 +181,8 @@ class Shuttle {
           err.message.includes('502 Bad Gateway') ||
           err.message.includes('ESOCKETTIMEDOUT') ||
           err.message.includes('internal service failure') ||
-          err.message.includes('Invalid JSON RPC response')
+          err.message.includes('Invalid JSON RPC response') ||
+          err.message.includes('handle request error')
         ) {
           if (this.errorCounter++ < 5) {
             // Delay 1000ms
