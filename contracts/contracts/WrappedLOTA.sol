98e0f040831f30ac10d7865f28d73365375caf1b
0xantZ <38260436+0xantman@users.noreply.github.com>
2021-07-14 02:05:44 +0200

    Fix typo

--- contracts/contracts/WrappedLOTA.sol
+++ contracts/contracts/WrappedLOTA.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.6.0;
 
 import './WrappedToken.sol';
 
-contract WrappedKRT is WrappedToken {
+contract WrappedLOTA is WrappedToken {
     constructor() public WrappedToken("Wrapped LOTA Token", "LOTA") {}
 }
